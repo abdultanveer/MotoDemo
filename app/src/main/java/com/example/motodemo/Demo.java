c220b0c49c2f75d2bbffe3045cd8a572b581b8b5
Abdul <abdul.tanveer@gmail.com>
2024-05-06 09:49:13 +0800

    java to kotlin

---
+++ app/src/main/java/com/example/motodemo/Demo.java
@@ -0,0 +1,8 @@
+package com.example.motodemo;
+
+public class Demo {
+    public static void main(String[] args) {
+        Student abdul = new Student("ansari",234,"ahladf",45678);
+        abdul.getAddress();
+    }
+}
--- app/src/main/java/com/example/motodemo/MainActivity.kt
+++ app/src/main/java/com/example/motodemo/MainActivity.kt
@@ -7,5 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+      //  Student abdul = new Student("ansari",234,"ahladf",45678);
+
+        var abdul = Student("ansari",123,"jakldhaf",7654)
     }
 }
