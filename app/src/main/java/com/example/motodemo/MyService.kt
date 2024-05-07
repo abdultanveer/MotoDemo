835825578503d293c580e3259bb2122f09d54bf2
Abdul <abdul.tanveer@gmail.com>
2024-05-07 16:22:48 +0800

    bound services

--- app/src/main/java/com/example/motodemo/MyService.kt
+++ app/src/main/java/com/example/motodemo/MyService.kt
@@ -2,10 +2,13 @@ package com.example.motodemo
 
 import android.app.Service
 import android.content.Intent
+import android.os.Binder
 import android.os.IBinder
 import android.util.Log
+import kotlin.random.Random
 
 class MyService : Service() {
+    private val mybinder = LocalBinder()
 
     override fun onCreate() {
         super.onCreate()
@@ -26,11 +29,31 @@ class MyService : Service() {
         Log.i(TAG,"service destroyed")
 
     }
+
+    private val mGenerator = Random(20)
+
+    fun getLocation():String{
+        return "penang"
+    }
+
+    val latestScore: Int
+        get() = mGenerator.nextInt(100)
+
+    //whenever a client[activity tries to bind to this service i'll return this binder
     override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
+        Log.i(TAG,"incoming binding requesting")
+        return  mybinder //2
     }
 
     companion object{
         var TAG = MyService::class.java.simpleName
+    }
+
+    inner class LocalBinder : Binder(){
+
+        //4
+        fun getInstanceMyService():MyService{
+            return  this@MyService
+        }
     }
 }
--- app/src/main/java/com/example/motodemo/SecondActivity.kt
+++ app/src/main/java/com/example/motodemo/SecondActivity.kt
@@ -1,8 +1,12 @@
 package com.example.motodemo
 
+import android.content.ComponentName
 import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
+import android.util.Log
 import androidx.lifecycle.lifecycleScope
 import com.example.motodemo.databinding.ActivityMainBinding
 import com.example.motodemo.databinding.ActivitySecondBinding
@@ -11,7 +15,7 @@ import kotlinx.coroutines.launch
 
 class SecondActivity : AppCompatActivity() {
     private lateinit var binding:ActivitySecondBinding
-
+    lateinit var mService: MyService
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
        // setContentView(R.layout.activity_second)
@@ -24,9 +28,40 @@ class SecondActivity : AppCompatActivity() {
 
         binding.btnStart.setOnClickListener { startMyService() }
         binding.btnStop.setOnClickListener { stopMyService() }
+        binding.btnBind.setOnClickListener { bindMyService() }
+        binding.btnUnbind.setOnClickListener { unbindMyService() }
     }
 
-    private fun stopMyService() {
+    private fun unbindMyService() {
+        TODO("Not yet implemented")
+    }
+
+    private fun bindMyService() {
+        var serviceIntent = Intent(this, MyService::class.java)
+
+        bindService(serviceIntent,serviceConnection, BIND_AUTO_CREATE)//1
+    }
+
+
+    private val serviceConnection = object : ServiceConnection {
+        //3
+        override fun onServiceConnected(p0: ComponentName?, mybinder: IBinder?) {
+            val binder = mybinder  as MyService.LocalBinder
+           // mService = MyService()
+            mService = binder.getInstanceMyService()
+           var score = mService.latestScore
+            Log.i(TAG,"score is --"+score)
+           var location = mService.getLocation()
+            Log.i(TAG,"location is --"+location)
+
+        }
+
+        override fun onServiceDisconnected(p0: ComponentName?) {
+            TODO("Not yet implemented")
+        }
+    }
+
+        private fun stopMyService() {
         var serviceIntent = Intent(this, MyService::class.java)
         stopService(serviceIntent)
     }
@@ -45,4 +80,8 @@ class SecondActivity : AppCompatActivity() {
             //_status.value = listResult
         }
         }
+
+    companion object{
+        var TAG = SecondActivity::class.java.simpleName
+    }
 }
